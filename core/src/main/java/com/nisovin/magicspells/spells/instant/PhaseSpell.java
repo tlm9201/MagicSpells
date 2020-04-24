ea50f364401a254e1be15810048b390ffc00333d
JasperLorelai <wolf.haci12@gmail.com>
2020-04-24 15:49:24 +0200

    PhaseSpell - added "non-phasable-blocks" blacklist

--- core/src/main/java/com/nisovin/magicspells/spells/instant/PhaseSpell.java
+++ core/src/main/java/com/nisovin/magicspells/spells/instant/PhaseSpell.java
@@ -9,6 +9,7 @@ import org.bukkit.block.Block;
 import org.bukkit.util.BlockIterator;
 import org.bukkit.entity.LivingEntity;
 
+import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.util.BlockUtils;
 import com.nisovin.magicspells.util.MagicConfig;
 import com.nisovin.magicspells.spells.InstantSpell;
@@ -16,30 +17,33 @@ import com.nisovin.magicspells.spelleffects.EffectPosition;
 
 public class PhaseSpell extends InstantSpell {
 
-	private List<Material> phasableBlocks;
+	private final int maxDistance;
+	private final String strCantPhase;
 
-	private String strCantPhase;
-
-	private int maxDistance;
+	private final List<Material> nonPhasableBlocks;
+	private final List<Material> phasableBlocks;
 
 	public PhaseSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
-
-		List<String> phasable = getConfigStringList("phasable-blocks", null);
-		phasableBlocks = new ArrayList<>();
-		if (phasable != null && !phasable.isEmpty()) {
-			for (String s : phasable) {
-				Material material = Material.getMaterial(s.toUpperCase());
-				if (material == null) continue;
-				phasableBlocks.add(material);
-			}
-		}
-
 		maxDistance = getConfigInt("max-distance", 15);
-
 		strCantPhase = getConfigString("str-cant-phase", "Unable to find place to phase to.");
-		
-		if (phasableBlocks.isEmpty()) phasableBlocks = null;
+		phasableBlocks = new ArrayList<>();
+		nonPhasableBlocks = new ArrayList<>();
+		processMaterials(phasableBlocks,"phasable-blocks");
+		processMaterials(nonPhasableBlocks,"non-phasable-blocks");
+	}
+
+	private void processMaterials(List<Material> materials, String path) {
+		List<String> matList = getConfigStringList(path, null);
+		if (matList == null || matList.isEmpty()) return;
+		for (String mat : matList) {
+			Material material = Material.getMaterial(mat.toUpperCase());
+			if (material == null) {
+				MagicSpells.error("PhaseSpell has an invalid material specified on '" + path + "': " + mat);
+				continue;
+			}
+			materials.add(material);
+		}
 	}
 
 	@Override
@@ -47,7 +51,7 @@ public class PhaseSpell extends InstantSpell {
 		if (state == SpellCastState.NORMAL) {
 			int r = Math.round(range * power);
 			int distance = Math.round(maxDistance * power);
-			
+
 			BlockIterator iter;
 			try {
 				iter = new BlockIterator(livingEntity, distance << 1);
@@ -59,38 +63,33 @@ public class PhaseSpell extends InstantSpell {
 			int i = 0;
 			Block start = null;
 			Location location = null;
-			
-			while (start == null && i++ < r << 1 && iter.hasNext()) {
+
+			while (i++ < r << 1 && iter.hasNext()) {
 				Block b = iter.next();
 				if (BlockUtils.isAir(b.getType())) continue;
 				if (livingEntity.getLocation().distanceSquared(b.getLocation()) >= r * r) continue;
 				start = b;
 				break;
 			}
-			
+
 			if (start != null) {
 				if (canPassThrough(start)) {
-					Block end = null;
-					while (end == null && i++ < distance << 1 && iter.hasNext()) {
-						Block b = iter.next();
-						if (BlockUtils.isAir(b.getType()) && BlockUtils.isAir(b.getRelative(0, 1, 0).getType())
-								&& livingEntity.getLocation().distanceSquared(b.getLocation()) < distance * distance) {
-							location = b.getLocation();
+					while (i++ < distance << 1 && iter.hasNext()) {
+						Block block = iter.next();
+						if (BlockUtils.isAir(block.getType()) && BlockUtils.isAir(block.getRelative(0, 1, 0).getType()) && livingEntity.getLocation().distanceSquared(block.getLocation()) < distance * distance) {
+							location = block.getLocation();
 							break;
 						}
-						if (!canPassThrough(b)) {
-							location = null;
-							break;
-						}
+						if (!canPassThrough(block)) break;
 					}
-				} else location = null;
+				}
 			}
-			
+
 			if (location == null) {
 				sendMessage(strCantPhase, livingEntity, args);
 				return PostCastAction.ALREADY_HANDLED;
 			}
-			
+
 			location.setX(location.getX() + 0.5);
 			location.setZ(location.getZ() + 0.5);
 			location.setPitch(livingEntity.getLocation().getPitch());
@@ -101,14 +100,10 @@ public class PhaseSpell extends InstantSpell {
 		}
 		return PostCastAction.HANDLE_NORMALLY;
 	}
-	
-	private boolean canPassThrough(Block block) {
-		if (phasableBlocks == null) return true;
-		if (phasableBlocks.isEmpty()) return true;
-		for (Material mat : phasableBlocks) {
-			if (mat.equals(block.getType())) return true;
-		}
-		return false;
-	}
 
+	private boolean canPassThrough(Block block) {
+		// Check only blacklist.
+		if (phasableBlocks.isEmpty()) return !nonPhasableBlocks.contains(block.getType());
+		return phasableBlocks.contains(block.getType()) && !nonPhasableBlocks.contains(block.getType());
+	}
 }
