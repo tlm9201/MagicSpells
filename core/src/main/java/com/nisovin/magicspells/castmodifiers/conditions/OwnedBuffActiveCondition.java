64c166a088eb69db0442a4e5095b2b5081b01a68
JasperLorelai <wolf.haci12@gmail.com>
2020-05-15 14:59:20 +0200

    Added OwnedBuffActive modifier condition

---
+++ core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/OwnedBuffActiveCondition.java
@@ -0,0 +1,42 @@
+package com.nisovin.magicspells.castmodifiers.conditions;
+
+import org.bukkit.Location;
+import org.bukkit.entity.LivingEntity;
+
+import com.nisovin.magicspells.Spell;
+import com.nisovin.magicspells.MagicSpells;
+import com.nisovin.magicspells.spells.BuffSpell;
+import com.nisovin.magicspells.castmodifiers.Condition;
+
+public class OwnedBuffActiveCondition extends Condition {
+
+    private BuffSpell buff;
+
+    @Override
+    public boolean setVar(String var) {
+        Spell spell = MagicSpells.getSpellByInternalName(var);
+        if (!(spell instanceof BuffSpell)) return false;
+        buff = (BuffSpell) spell;
+        return true;
+    }
+
+    @Override
+    public boolean check(LivingEntity caster) {
+        return isOwned(caster, caster);
+    }
+
+    @Override
+    public boolean check(LivingEntity caster, LivingEntity target) {
+        return isOwned(caster, target);
+    }
+
+    @Override
+    public boolean check(LivingEntity caster, Location location) {
+        return false;
+    }
+
+    private boolean isOwned(LivingEntity caster, LivingEntity target) {
+        if (target == null) return false;
+        return buff.isActiveAndNotExpired(target) && buff.getLastCaster(target).equals(caster);
+    }
+}
