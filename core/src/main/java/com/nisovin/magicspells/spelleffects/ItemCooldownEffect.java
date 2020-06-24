7c31b17638ab608ac4b23cff612cf39ffc1f935f
Chronoken <norwinofficial@gmail.com>
2020-06-24 14:05:40 +0200

    Added support for MagicItems

--- core/src/main/java/com/nisovin/magicspells/spelleffects/ItemCooldownEffect.java
+++ core/src/main/java/com/nisovin/magicspells/spelleffects/ItemCooldownEffect.java
@@ -5,8 +5,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.configuration.ConfigurationSection;
 
-import com.nisovin.magicspells.util.Util;
 import com.nisovin.magicspells.util.TimeUtil;
+import com.nisovin.magicspells.util.magicitems.MagicItem;
+import com.nisovin.magicspells.util.magicitems.MagicItems;
 
 public class ItemCooldownEffect extends SpellEffect {
 
@@ -16,13 +17,15 @@ public class ItemCooldownEffect extends SpellEffect {
 
 	@Override
 	protected void loadFromConfig(ConfigurationSection config) {
-		item = Util.getItemStackFromString(config.getString("item", "stone"));
+		MagicItem magicItem = MagicItems.getMagicItemFromString(config.getString("item", "stone"));
+		if (magicItem != null) item = magicItem.getItemStack();
 		duration = config.getInt("duration", TimeUtil.TICKS_PER_SECOND);
 	}
 	
 	@Override
 	protected Runnable playEffectEntity(Entity entity) {
 		if (!(entity instanceof Player)) return null;
+		if (item == null) return null;
 		((Player) entity).setCooldown(item.getType(), duration);
 		return null;
 	}
