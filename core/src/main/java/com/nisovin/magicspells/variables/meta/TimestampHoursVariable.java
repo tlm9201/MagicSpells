7d0d8f725e970da30326e6e4699c5e9cece3742d
Dragon <silentnate347@outlook.com>
2020-07-18 13:18:16 -0400

    Added new Special Variables
    Variables are...
    - "meta_timestamp_days"
    - "meta_timestamp_hours"
    - "meta_timestamp_minutes"
    - "meta_timestamp_seconds"
    
    These special variables allow you to measure large distances of time without ticking such as getting the last login date of a player and seeing hour many days, hours, minutes, or seconds it has been since.

---
+++ core/src/main/java/com/nisovin/magicspells/variables/meta/TimestampHoursVariable.java
@@ -0,0 +1,16 @@
+package com.nisovin.magicspells.variables.meta;
+
+import com.nisovin.magicspells.variables.MetaVariable;
+
+import java.time.Instant;
+
+public class TimestampHoursVariable extends MetaVariable {
+
+    @Override
+    public double getValue(String player) {
+        Instant instant = Instant.now();
+        long mins = instant.getEpochSecond();
+        return Math.floor(mins/60/60);
+    }
+
+}
