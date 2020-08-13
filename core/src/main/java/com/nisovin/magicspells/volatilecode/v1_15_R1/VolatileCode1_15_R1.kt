609ec8a8a0a6f546bd1f3d10fed69eb78879525a
JasperLorelai <wolf.haci12@gmail.com>
2020-08-13 20:50:59 +0200

    Refactor due to MagicSpells#sendMessage and Spell#sendMessageNear not using the Util#colorize method

--- core/src/main/java/com/nisovin/magicspells/volatilecode/v1_15_R1/VolatileCode1_15_R1.kt
+++ core/src/main/java/com/nisovin/magicspells/volatilecode/v1_15_R1/VolatileCode1_15_R1.kt
@@ -251,7 +251,7 @@ class VolatileCode1_15_R1: VolatileCodeHandle {
     }
 
     override fun colorize(message: String?): String {
-        return message.toString()
+        return org.bukkit.ChatColor.translateAlternateColorCodes('&', message.toString())
     }
 
     private fun setTexture(profile: GameProfile, texture: String, signature: String?): GameProfile {
