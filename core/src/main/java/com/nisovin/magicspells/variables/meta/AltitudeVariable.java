a9ed61e6aca68dfca674a02ee874e4a9bd9602da
JasperLorelai <wolf.haci12@gmail.com>
2020-10-18 17:44:48 +0200

    The highest point isn't always below the player so we have to loop blocks

--- core/src/main/java/com/nisovin/magicspells/variables/meta/AltitudeVariable.java
+++ core/src/main/java/com/nisovin/magicspells/variables/meta/AltitudeVariable.java
@@ -13,10 +13,20 @@ public class AltitudeVariable extends MetaVariable {
     public double getValue(String player) {
         Player p = PlayerNameUtils.getPlayerExact(player);
         if (p == null) return 0;
+        World world = p.getWorld();
+        Location location = p.getLocation().clone().subtract(0,1,0);
+        int x = location.getBlockX();
+        int y = location.getBlockY();
+        int z = location.getBlockZ();
 
-        Location location = p.getLocation();
-        World world = location.getWorld();
-        if (world != null) return location.getY() - world.getHighestBlockYAt(location);
+        // Try to avoid looping through block below the player.
+        int highestPoint = world.getHighestBlockYAt(location);
+        if (highestPoint < y) return y - highestPoint;
+
+        for (int i = y; i > 0; i--) {
+            if (world.getBlockAt(x, i, z).getType().isAir()) continue;
+            return y - i;
+        }
         return 0;
     }
 
