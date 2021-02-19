6a8edb192a30c6283911921103a1fd925d3a9cdd
TonytheMacaroni <tonythemacaroni123@gmail.com>
2021-02-18 22:02:39 -0500

    Added LeftClickItemListener

---
+++ core/src/main/java/com/nisovin/magicspells/spells/passive/LeftClickItemListener.java
@@ -0,0 +1,75 @@
+package com.nisovin.magicspells.spells.passive;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import org.bukkit.event.Event;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import com.nisovin.magicspells.MagicSpells;
+import com.nisovin.magicspells.util.OverridePriority;
+import com.nisovin.magicspells.util.magicitems.MagicItems;
+import com.nisovin.magicspells.util.magicitems.MagicItemData;
+import com.nisovin.magicspells.spells.passive.util.PassiveListener;
+
+// Trigger variable of a pipe separated list of items to accept
+public class LeftClickItemListener extends PassiveListener {
+
+    private final Set<MagicItemData> items = new HashSet<>();
+
+    @Override
+    public void initialize(String var) {
+        if (var == null || var.isEmpty()) return;
+
+        String[] split = var.split("\\|");
+        for (String s : split) {
+            s = s.trim();
+
+            MagicItemData itemData = MagicItems.getMagicItemDataFromString(s);
+            if (itemData == null) {
+                MagicSpells.error("Invalid magic item '" + s + "' in leftclickitem trigger on passive spell '" + passiveSpell.getInternalName() + "'");
+                continue;
+            }
+
+            items.add(itemData);
+        }
+    }
+
+    @OverridePriority
+    @EventHandler
+    public void onLeftClick(PlayerInteractEvent event) {
+        if (event.getAction() != Action.LEFT_CLICK_AIR && event.getAction() != Action.LEFT_CLICK_BLOCK) return;
+        if (!isCancelStateOk(isCancelled(event))) return;
+        if (!event.hasItem()) return;
+
+        Player caster = event.getPlayer();
+        if (!hasSpell(event.getPlayer()) || !canTrigger(caster)) return;
+
+        if (!items.isEmpty()) {
+            ItemStack item = event.getItem();
+            if (item == null) return;
+
+            MagicItemData itemData = MagicItems.getMagicItemDataFromItemStack(item);
+            if (itemData == null || !contains(itemData)) return;
+        }
+
+        boolean casted = passiveSpell.activate(event.getPlayer());
+        if (cancelDefaultAction(casted)) event.setCancelled(true);
+    }
+
+    private boolean contains(MagicItemData itemData) {
+        for (MagicItemData data : items) {
+            if (data.matches(itemData)) return true;
+        }
+        return false;
+    }
+
+    private boolean isCancelled(PlayerInteractEvent event) {
+        return event.useInteractedBlock() == Event.Result.DENY && event.useItemInHand() == Event.Result.DENY;
+    }
+
+}
