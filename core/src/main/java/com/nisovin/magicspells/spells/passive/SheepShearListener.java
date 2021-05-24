1abf5f9bcaedf0fb315e46e03a2983469dcdcd37
tonythemacaroni <tonythemacaroni123@gmail.com>
2021-05-24 19:52:24 -0400

    Allow multiple dye colors for sheepshear trigger

--- core/src/main/java/com/nisovin/magicspells/spells/passive/SheepShearListener.java
+++ core/src/main/java/com/nisovin/magicspells/spells/passive/SheepShearListener.java
@@ -4,10 +4,12 @@ import java.util.EnumSet;
 
 import org.bukkit.DyeColor;
 import org.bukkit.entity.Sheep;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerShearEntityEvent;
 
+import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.util.OverridePriority;
 import com.nisovin.magicspells.spells.passive.util.PassiveListener;
 
@@ -15,31 +17,37 @@ import com.nisovin.magicspells.spells.passive.util.PassiveListener;
 public class SheepShearListener extends PassiveListener {
 
 	private final EnumSet<DyeColor> dyeColors = EnumSet.noneOf(DyeColor.class);
-	
+
 	@Override
 	public void initialize(String var) {
 		if (var == null || var.isEmpty()) return;
 
-		try {
-			DyeColor color = DyeColor.valueOf(var.toUpperCase());
-			dyeColors.add(color);
-		} catch (Exception e) {
-			// ignored
+		String[] split = var.split(",");
+		for (String s : split) {
+			try {
+				DyeColor color = DyeColor.valueOf(s.trim().toUpperCase());
+				dyeColors.add(color);
+			} catch (IllegalArgumentException e) {
+				MagicSpells.error("Invalid dye color '" + s + "' in sheepshear trigger on passive spell '" + passiveSpell.getInternalName() + "'");
+			}
 		}
 	}
-	
+
 	@OverridePriority
 	@EventHandler
 	public void onSheepShear(PlayerShearEntityEvent event) {
-		if (!(event.getEntity() instanceof Sheep)) return;
-		Sheep sheep = (Sheep) event.getEntity();
-		Player pl = event.getPlayer();
-		if (!hasSpell(pl)) return;
-
-		if (!dyeColors.isEmpty() && !dyeColors.contains(sheep.getColor())) return;
-
 		if (!isCancelStateOk(event.isCancelled())) return;
-		boolean casted = passiveSpell.activate(pl);
+
+		Entity entity = event.getEntity();
+		if (!(entity instanceof Sheep)) return;
+
+		Player caster = event.getPlayer();
+		if (!hasSpell(caster) || !canTrigger(caster)) return;
+
+		Sheep target = (Sheep) event.getEntity();
+		if (!dyeColors.isEmpty() && !dyeColors.contains(target.getColor())) return;
+
+		boolean casted = passiveSpell.activate(caster);
 		if (cancelDefaultAction(casted)) event.setCancelled(true);
 	}
 
