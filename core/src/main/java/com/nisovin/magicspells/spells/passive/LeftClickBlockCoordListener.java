86e7363cb49f251c5b492b46e63a5f2244f5d110
tonythemacaroni <tonythemacaroni123@gmail.com>
2021-05-24 19:04:03 -0400

    Cleanup leftclickblockcoord trigger

--- core/src/main/java/com/nisovin/magicspells/spells/passive/LeftClickBlockCoordListener.java
+++ core/src/main/java/com/nisovin/magicspells/spells/passive/LeftClickBlockCoordListener.java
@@ -1,9 +1,11 @@
 package com.nisovin.magicspells.spells.passive;
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.event.Event;
-import org.bukkit.event.EventHandler;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.nisovin.magicspells.MagicSpells;
@@ -18,7 +20,7 @@ import com.nisovin.magicspells.spells.passive.util.PassiveListener;
 public class LeftClickBlockCoordListener extends PassiveListener {
 
 	private MagicLocation magicLocation;
-	
+
 	@Override
 	public void initialize(String var) {
 		String[] split = var.split(";");
@@ -28,24 +30,31 @@ public class LeftClickBlockCoordListener extends PassiveListener {
 				String world = data[0];
 				int x = Integer.parseInt(data[1]);
 				int y = Integer.parseInt(data[2]);
-				int z = Integer.parseInt(data[3]);				
+				int z = Integer.parseInt(data[3]);
 				magicLocation = new MagicLocation(world, x, y, z);
 			} catch (NumberFormatException e) {
-				MagicSpells.error("Invalid coords on leftClickBlockCoord trigger for spell '" + passiveSpell.getInternalName() + "'");
+				MagicSpells.error("Invalid coords on leftclickblockcoord trigger for spell '" + passiveSpell.getInternalName() + "'");
 			}
 		}
 	}
-	
+
 	@OverridePriority
 	@EventHandler
 	public void onLeftClick(PlayerInteractEvent event) {
 		if (event.getAction() != Action.LEFT_CLICK_BLOCK) return;
+		if (!isCancelStateOk(isCancelled(event))) return;
+
+		Block block = event.getClickedBlock();
+		if (block == null) return;
+
+		Player caster = event.getPlayer();
+		if (!hasSpell(caster) || !canTrigger(caster)) return;
+
 		Location location = event.getClickedBlock().getLocation();
 		MagicLocation loc = new MagicLocation(location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
 		if (!magicLocation.equals(loc)) return;
-		if (!isCancelStateOk(isCancelled(event))) return;
-		if (!hasSpell(event.getPlayer())) return;
-		boolean casted = passiveSpell.activate(event.getPlayer(), location.add(0.5, 0.5, 0.5));
+
+		boolean casted = passiveSpell.activate(caster, location.add(0.5, 0.5, 0.5));
 		if (cancelDefaultAction(casted)) event.setCancelled(true);
 	}
 
