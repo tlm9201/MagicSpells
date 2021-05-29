1d50894d8a7324b603ca96fe89014f3c71caafba
tonythemacaroni <tonythemacaroni123@gmail.com>
2021-05-29 13:01:44 -0400

    Add support for multiple locations

--- core/src/main/java/com/nisovin/magicspells/spells/passive/LeftClickBlockCoordListener.java
+++ core/src/main/java/com/nisovin/magicspells/spells/passive/LeftClickBlockCoordListener.java
@@ -1,5 +1,8 @@
 package com.nisovin.magicspells.spells.passive;
 
+import java.util.Set;
+import java.util.HashSet;
+
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.event.Event;
@@ -19,11 +22,12 @@ import com.nisovin.magicspells.spells.passive.util.PassiveListener;
 // And x, y, and z are integers
 public class LeftClickBlockCoordListener extends PassiveListener {
 
-	private MagicLocation magicLocation;
+	private final Set<MagicLocation> locations = new HashSet<>();
 
 	@Override
 	public void initialize(String var) {
 		String[] split = var.split(";");
+
 		for (String s : split) {
 			try {
 				String[] data = s.split(",");
@@ -31,7 +35,9 @@ public class LeftClickBlockCoordListener extends PassiveListener {
 				int x = Integer.parseInt(data[1]);
 				int y = Integer.parseInt(data[2]);
 				int z = Integer.parseInt(data[3]);
-				magicLocation = new MagicLocation(world, x, y, z);
+
+				MagicLocation location = new MagicLocation(world, x, y, z);
+				locations.add(location);
 			} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
 				MagicSpells.error("Invalid coords on leftclickblockcoord trigger for spell '" + passiveSpell.getInternalName() + "'");
 			}
@@ -52,7 +58,7 @@ public class LeftClickBlockCoordListener extends PassiveListener {
 
 		Location location = event.getClickedBlock().getLocation();
 		MagicLocation loc = new MagicLocation(location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
-		if (!magicLocation.equals(loc)) return;
+		if (!locations.contains(loc)) return;
 
 		boolean casted = passiveSpell.activate(caster, location.add(0.5, 0.5, 0.5));
 		if (cancelDefaultAction(casted)) event.setCancelled(true);
--- core/src/main/java/com/nisovin/magicspells/spells/passive/RightClickBlockCoordListener.java
+++ core/src/main/java/com/nisovin/magicspells/spells/passive/RightClickBlockCoordListener.java
@@ -1,5 +1,8 @@
 package com.nisovin.magicspells.spells.passive;
 
+import java.util.Set;
+import java.util.HashSet;
+
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.event.Event;
@@ -18,11 +21,12 @@ import com.nisovin.magicspells.spells.passive.util.PassiveListener;
 // Where "world" is a string and x, y, and z are integers
 public class RightClickBlockCoordListener extends PassiveListener {
 
-	private MagicLocation magicLocation;
+	private final Set<MagicLocation> locations = new HashSet<>();
 
 	@Override
 	public void initialize(String var) {
 		String[] split = var.split(";");
+
 		for (String s : split) {
 			try {
 				String[] data = s.split(",");
@@ -30,7 +34,9 @@ public class RightClickBlockCoordListener extends PassiveListener {
 				int x = Integer.parseInt(data[1]);
 				int y = Integer.parseInt(data[2]);
 				int z = Integer.parseInt(data[3]);
-				magicLocation = new MagicLocation(world, x, y, z);
+
+				MagicLocation location = new MagicLocation(world, x, y, z);
+				locations.add(location);
 			} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
 				MagicSpells.error("Invalid coords on rightclickblockcoord trigger for spell '" + passiveSpell.getInternalName() + "'");
 			}
@@ -51,7 +57,7 @@ public class RightClickBlockCoordListener extends PassiveListener {
 
 		Location location = event.getClickedBlock().getLocation();
 		MagicLocation loc = new MagicLocation(location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
-		if (!magicLocation.equals(loc)) return;
+		if (!locations.contains(loc)) return;
 
 		boolean casted = passiveSpell.activate(caster, location.add(0.5, 0.5, 0.5));
 		if (cancelDefaultAction(casted)) event.setCancelled(true);
