80ea9fda9d42d2a546279066a89da3a56d4059e6
Chronoken <norwinofficial@gmail.com>
2021-06-24 18:55:14 +0200

    Initial 1.17 commit

--- core/src/main/java/com/nisovin/magicspells/volatilecode/v1_17_R1/VolatileCode1_17_R1.kt
+++ core/src/main/java/com/nisovin/magicspells/volatilecode/v1_17_R1/VolatileCode1_17_R1.kt
@@ -1,5 +1,4 @@
-package com.nisovin.magicspells.volatilecode.v1_16_R3
-/*
+package com.nisovin.magicspells.volatilecode.v1_17_R1
 
 import java.lang.reflect.Field
 
@@ -11,12 +10,12 @@ import org.bukkit.inventory.*
 import org.bukkit.util.Vector
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
-import org.bukkit.craftbukkit.v1_16_R3.entity.*
+import org.bukkit.craftbukkit.v1_17_R1.entity.*
 import org.bukkit.persistence.PersistentDataType
-import org.bukkit.craftbukkit.v1_16_R3.CraftWorld
+import org.bukkit.craftbukkit.v1_17_R1.CraftWorld
 import org.bukkit.event.entity.ExplosionPrimeEvent
-import org.bukkit.craftbukkit.v1_16_R3.CraftServer
-import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftItemStack
+import org.bukkit.craftbukkit.v1_17_R1.CraftServer
+import org.bukkit.craftbukkit.v1_17_R1.inventory.CraftItemStack
 
 import com.nisovin.magicspells.util.*
 import com.nisovin.magicspells.MagicSpells
@@ -25,12 +24,21 @@ import com.nisovin.magicspells.util.compat.EventUtil
 import com.nisovin.magicspells.volatilecode.VolatileCodeHandle
 
 import net.md_5.bungee.api.ChatColor
+import net.minecraft.network.chat.ChatMessage
+import net.minecraft.network.protocol.game.*
+import net.minecraft.network.syncher.DataWatcherObject
 
-import net.minecraft.server.v1_16_R3.*
+import net.minecraft.server.*
+import net.minecraft.world.entity.EntityLiving
+import net.minecraft.world.entity.EntityTypes
+import net.minecraft.world.entity.boss.enderdragon.EntityEnderDragon
+import net.minecraft.world.entity.item.EntityFallingBlock
+import net.minecraft.world.entity.item.EntityTNTPrimed
+import net.minecraft.world.phys.Vec3D
 
-private typealias nmsItemStack = net.minecraft.server.v1_16_R3.ItemStack
+private typealias nmsItemStack = net.minecraft.world.item.ItemStack
 
-class VolatileCode1_16_R3: VolatileCodeHandle {
+class VolatileCode1_17_R1: VolatileCodeHandle {
 
     private var entityFallingBlockFallHurtAmountField: Field? = null
     private var entityFallingBlockFallHurtMaxField: Field? = null
@@ -46,7 +54,7 @@ class VolatileCode1_16_R3: VolatileCodeHandle {
             this.entityFallingBlockFallHurtMaxField = EntityFallingBlock::class.java.getDeclaredField("fallHurtMax")
             this.entityFallingBlockFallHurtMaxField!!.isAccessible = true
 
-            this.craftMetaSkullClass = Class.forName("org.bukkit.craftbukkit.v1_16_R3.inventory.CraftMetaSkull")
+            this.craftMetaSkullClass = Class.forName("org.bukkit.craftbukkit.v1_17_R1.inventory.CraftMetaSkull")
             this.craftMetaSkullProfileField = this.craftMetaSkullClass!!.getDeclaredField("profile")
             this.craftMetaSkullProfileField!!.isAccessible = true
 
@@ -54,13 +62,13 @@ class VolatileCode1_16_R3: VolatileCodeHandle {
             entityLivingPotionEffectColorField.isAccessible = true;
             this.entityLivingPotionEffectColor = entityLivingPotionEffectColorField.get(null) as DataWatcherObject<Int>
         } catch (e: Exception) {
-            MagicSpells.error("THIS OCCURRED WHEN CREATING THE VOLATILE CODE HANDLE FOR 1.16.5, THE FOLLOWING ERROR IS MOST LIKELY USEFUL IF YOU'RE RUNNING THE LATEST VERSION OF MAGICSPELLS.")
+            MagicSpells.error("THIS OCCURRED WHEN CREATING THE VOLATILE CODE HANDLE FOR 1.17, THE FOLLOWING ERROR IS MOST LIKELY USEFUL IF YOU'RE RUNNING THE LATEST VERSION OF MAGICSPELLS.")
             e.printStackTrace()
         }
     }
 
     override fun addPotionGraphicalEffect(entity: LivingEntity, color: Int, duration: Int) {
-        val livingEntity = (entity as CraftLivingEntity).handle;
+        /*val livingEntity = (entity as CraftLivingEntity).handle;
         val dataWatcher = livingEntity.dataWatcher;
         dataWatcher.set(entityLivingPotionEffectColor, color)
         if (duration > 0) {
@@ -71,18 +79,18 @@ class VolatileCode1_16_R3: VolatileCodeHandle {
                 }
                 dataWatcher.set(entityLivingPotionEffectColor, c)
             }, duration)
-        }
+        } */
     }
 
     override fun sendFakeSlotUpdate(player: Player, slot: Int, item: ItemStack?) {
-        val nmsItem: nmsItemStack?
+        /*val nmsItem: nmsItemStack?
         if (item != null) {
             nmsItem = CraftItemStack.asNMSCopy(item)
         } else {
             nmsItem = null
         }
         val packet = PacketPlayOutSetSlot(0, slot.toShort() + 36, nmsItem!!)
-        (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
+        (player as CraftPlayer).handle.playerConnection.sendPacket(packet) */
     }
 
     override fun simulateTnt(target: Location, source: LivingEntity, explosionSize: Float, fire: Boolean): Boolean {
@@ -110,7 +118,7 @@ class VolatileCode1_16_R3: VolatileCodeHandle {
     }
 
     override fun playDragonDeathEffect(location: Location) {
-        val dragon = EntityEnderDragon(EntityTypes.ENDER_DRAGON, (location.world as CraftWorld).handle)
+        /*val dragon = EntityEnderDragon(EntityTypes.v, (location.world as CraftWorld).handle)
         dragon.setPositionRotation(location.x, location.y, location.z, location.yaw, 0f)
 
         val packet24 = PacketPlayOutSpawnEntityLiving(dragon)
@@ -132,12 +140,12 @@ class VolatileCode1_16_R3: VolatileCodeHandle {
                     (player as CraftPlayer).handle.playerConnection.sendPacket(packet29)
                 }
             }
-        }, 250)
+        }, 250)*/
     }
 
     override fun setClientVelocity(player: Player, velocity: Vector) {
-        val packet = PacketPlayOutEntityVelocity(player.entityId, Vec3D(velocity.x, velocity.y, velocity.z))
-        (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
+        /*val packet = PacketPlayOutEntityVelocity(player.entityId, Vec3D(velocity.x, velocity.y, velocity.z))
+        (player as CraftPlayer).handle.playerConnection.sendPacket(packet)*/
     }
 
     override fun colorize(message: String?): String? {
@@ -145,7 +153,7 @@ class VolatileCode1_16_R3: VolatileCodeHandle {
         val buffer = StringBuffer()
         while (matcher.find()) {
             try {
-                matcher.appendReplacement(buffer, ChatColor.of(matcher.group(1).toUpperCase()).toString())
+                matcher.appendReplacement(buffer, ChatColor.of(matcher.group(1).uppercase()).toString())
             } catch (exception: IllegalArgumentException) {
                 // ignored
             }
@@ -165,11 +173,11 @@ class VolatileCode1_16_R3: VolatileCodeHandle {
     }
 
     override fun setInventoryTitle(player: Player, title: String) {
-        val entityPlayer = (player as CraftPlayer).handle
+        /*val entityPlayer = (player as CraftPlayer).handle
         val container = entityPlayer.activeContainer
         val packet = PacketPlayOutOpenWindow(container.windowId, container.type, ChatMessage(title))
         entityPlayer.playerConnection.sendPacket(packet)
-        entityPlayer.updateInventory(container)
+        entityPlayer.updateInventory(container)*/
     }
 
     override fun createSmithingRecipe(namespaceKey: NamespacedKey, result: ItemStack, base: Material, addition: Material): Recipe {
@@ -177,4 +185,3 @@ class VolatileCode1_16_R3: VolatileCodeHandle {
     }
 
 }
-*/
