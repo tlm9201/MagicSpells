f83e571532d583f4a090e9b81a137d4944cea941
tonythemacaroni <tonythemacaroni123@gmail.com>
2021-12-17 22:12:29 -0500

    Function support for PermissionSpell duration

--- core/src/main/java/com/nisovin/magicspells/spells/PermissionSpell.java
+++ core/src/main/java/com/nisovin/magicspells/spells/PermissionSpell.java
@@ -6,29 +6,35 @@ import org.bukkit.entity.LivingEntity;
 
 import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.util.MagicConfig;
+import com.nisovin.magicspells.util.config.ConfigData;
 import com.nisovin.magicspells.spelleffects.EffectPosition;
 
 public class PermissionSpell extends InstantSpell {
 
-	private int duration;
+	private ConfigData<Integer> duration;
 	
 	private List<String> permissionNodes;
 	
 	public PermissionSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 		
-		duration = getConfigInt("duration", 0);
+		duration = getConfigDataInt("duration", 0);
 		permissionNodes = getConfigStringList("permission-nodes", null);
 	}
 
 	@Override
 	public PostCastAction castSpell(LivingEntity caster, SpellCastState state, float power, String[] args) {
-		if (state == SpellCastState.NORMAL && duration > 0 && permissionNodes != null) {
+		if (state == SpellCastState.NORMAL && permissionNodes != null) {
+			int duration = this.duration.get(caster, null, power, args);
+			if (duration <= 0) return PostCastAction.HANDLE_NORMALLY;
+
 			for (String node : permissionNodes) {
 				caster.addAttachment(MagicSpells.plugin, node, true, duration);
 			}
+
 			playSpellEffects(EffectPosition.CASTER, caster);
 		}
+
 		return PostCastAction.HANDLE_NORMALLY;
 	}
 
