8976ea2e7cfeebf0a51cb342e09748005967b73e
tonythemacaroni <tonythemacaroni123@gmail.com>
2022-01-05 11:15:09 -0500

    Cleanup, fix error messaging

--- core/src/main/java/com/nisovin/magicspells/spells/passive/EntityTargetListener.java
+++ core/src/main/java/com/nisovin/magicspells/spells/passive/EntityTargetListener.java
@@ -1,56 +1,49 @@
 package com.nisovin.magicspells.spells.passive;
 
-import com.nisovin.magicspells.MagicSpells;
-import com.nisovin.magicspells.spells.passive.util.PassiveListener;
-import com.nisovin.magicspells.util.OverridePriority;
-import org.bukkit.entity.LivingEntity;
+import java.util.Set;
+import java.util.EnumSet;
+
 import org.bukkit.event.EventHandler;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.EntityTargetEvent;
 
-import java.util.EnumSet;
+import com.nisovin.magicspells.MagicSpells;
+import com.nisovin.magicspells.util.OverridePriority;
+import com.nisovin.magicspells.spells.passive.util.PassiveListener;
 
 import static org.bukkit.event.entity.EntityTargetEvent.TargetReason;
 
 public class EntityTargetListener extends PassiveListener {
 
-    private final EnumSet<TargetReason> targetReasons = EnumSet.noneOf(TargetReason.class);
+	private final Set<TargetReason> targetReasons = EnumSet.noneOf(TargetReason.class);
 
-    @Override
-    public void initialize(String var) {
-        if (var == null || var.isEmpty()) return;
+	@Override
+	public void initialize(String var) {
+		if (var == null || var.isEmpty()) return;
 
-        String[] split = var.split("\\|");
-        for (String s : split) {
-            s = s.trim();
+		String[] split = var.split("\\|");
+		for (String s : split) {
+			try {
+				TargetReason reason = TargetReason.valueOf(s.trim().toUpperCase());
+				targetReasons.add(reason);
+			} catch (IllegalArgumentException e) {
+				MagicSpells.error("Invalid target reason'" + s + "' in entitytarget trigger on passive spell '" + passiveSpell.getInternalName() + "'.");
+			}
+		}
+	}
 
-            boolean isTargetReason = false;
-            for (TargetReason r : TargetReason.values()) {
-                if (!s.equalsIgnoreCase(r.name())) continue;
+	@OverridePriority
+	@EventHandler
+	public void onEntityTarget(EntityTargetEvent event) {
+		if (!isCancelStateOk(event.isCancelled())) return;
+		if (!(event.getEntity() instanceof LivingEntity caster)) return;
+		if (!(event.getTarget() instanceof LivingEntity target)) return;
 
-                targetReasons.add(r);
-                isTargetReason = true;
-                break;
-            }
-            if (isTargetReason) continue;
+		if (!targetReasons.isEmpty() && !targetReasons.contains(event.getReason())) return;
+		if (!hasSpell(caster) || !canTrigger(caster)) return;
 
-            if (targetReasons == null) {
-                MagicSpells.error("Invalid target reason'" + s + "' in entity target trigger on passive spell '" + passiveSpell.getInternalName() + "'");
-                continue;
-            }
-        }
-    }
-
-    @OverridePriority
-    @EventHandler
-    public void onEntityTarget(EntityTargetEvent event) {
-        LivingEntity target = (LivingEntity) event.getTarget();
-        LivingEntity caster = (LivingEntity) event.getEntity();
-        if (!isCancelStateOk(event.isCancelled())) return;
-        if (!hasSpell(caster) || !canTrigger(caster)) return;
-        if (!targetReasons.isEmpty() && !targetReasons.contains(event.getReason())) return;
-
-        boolean casted = passiveSpell.activate(caster, target);
-        if (cancelDefaultAction(casted)) event.setCancelled(true);
-    }
+		boolean casted = passiveSpell.activate(caster, target);
+		if (cancelDefaultAction(casted)) event.setCancelled(true);
+	}
 
 }
