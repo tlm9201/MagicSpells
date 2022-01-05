55c3c6603a3ac515a9a9baec52f4b5cc899ec169
tonythemacaroni <tonythemacaroni123@gmail.com>
2022-01-05 12:16:03 -0500

    Cleanup

--- core/src/main/java/com/nisovin/magicspells/util/managers/PassiveManager.java
+++ core/src/main/java/com/nisovin/magicspells/util/managers/PassiveManager.java
@@ -79,7 +79,6 @@ public class PassiveManager {
 		addListener("dropitem", DropItemListener.class);
 		addListener("enterbed", EnterBedListener.class);
 		addListener("dismount", DismountListener.class);
-		addListener("mount", MountListener.class);
 		addListener("enchant", EnchantListener.class);
 		addListener("entitytarget", EntityTargetListener.class);
 		addListener("equip", EquipListener.class);
