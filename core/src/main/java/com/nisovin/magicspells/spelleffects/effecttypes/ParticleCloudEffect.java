806745fa8aaf8c0fed4659d8690f0f419b247241
tonythemacaroni <tonythemacaroni123@gmail.com>
2022-02-21 14:12:43 -0500

    Use functionality from ParticlesEffect

--- core/src/main/java/com/nisovin/magicspells/spelleffects/effecttypes/ParticleCloudEffect.java
+++ core/src/main/java/com/nisovin/magicspells/spelleffects/effecttypes/ParticleCloudEffect.java
@@ -2,26 +2,17 @@ package com.nisovin.magicspells.spelleffects.effecttypes;
 
 import org.bukkit.Color;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Particle;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.block.data.BlockData;
-import org.bukkit.Particle.DustOptions;
 import org.bukkit.entity.AreaEffectCloud;
 import org.bukkit.configuration.ConfigurationSection;
 
 import com.nisovin.magicspells.util.SpellData;
 import com.nisovin.magicspells.util.config.ConfigData;
-import com.nisovin.magicspells.spelleffects.SpellEffect;
 import com.nisovin.magicspells.util.config.ConfigDataUtil;
 
-public class ParticleCloudEffect extends SpellEffect {
+public class ParticleCloudEffect extends ParticlesEffect {
 
 	private ConfigData<Particle> particle;
-
-	private ConfigData<BlockData> blockData;
-	private ConfigData<Material> material;
-	private ConfigData<DustOptions> dustOptions;
 
 	private ConfigData<Integer> color;
 	private ConfigData<Integer> duration;
@@ -33,10 +24,6 @@ public class ParticleCloudEffect extends SpellEffect {
 	@Override
 	public void loadFromConfig(ConfigurationSection config) {
 		particle = ConfigDataUtil.getParticle(config, "particle-name", Particle.EXPLOSION_NORMAL);
-
-		blockData = ConfigDataUtil.getBlockData(config, "material", null);
-		material = ConfigDataUtil.getEnum(config, "material", Material.class, null);
-		dustOptions = ConfigDataUtil.getDustOptions(config, "color", "size", new DustOptions(Color.RED, 1));
 
 		color = ConfigDataUtil.getInteger(config, "color", 0xFF0000);
 		duration = ConfigDataUtil.getInteger(config, "duration", 60);
@@ -57,12 +44,7 @@ public class ParticleCloudEffect extends SpellEffect {
 		cloud.setDuration(duration.get(data));
 		cloud.setRadiusPerTick(radiusPerTick.get(data));
 
-		if (particle.getDataType() == ItemStack.class) {
-			Material material = this.material.get(data);
-			cloud.setParticle(particle, material == null || !material.isItem() ? null : new ItemStack(material));
-		} else if (blockData != null && particle.getDataType() == BlockData.class) cloud.setParticle(particle, blockData.get(data));
-		else if (particle.getDataType() == DustOptions.class) cloud.setParticle(particle, dustOptions.get(data));
-		else cloud.setParticle(particle);
+		cloud.setParticle(particle, getParticleData(particle, location, data));
 
 		return null;
 	}
