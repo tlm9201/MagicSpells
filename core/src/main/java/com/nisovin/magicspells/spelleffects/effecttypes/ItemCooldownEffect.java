860c7192e5facec9806ba0cd61e34a0225feaa74
tonythemacaroni <tonythemacaroni123@gmail.com>
2022-03-04 17:35:34 -0500

    Allow replacement of item

--- core/src/main/java/com/nisovin/magicspells/spelleffects/effecttypes/ItemCooldownEffect.java
+++ core/src/main/java/com/nisovin/magicspells/spelleffects/effecttypes/ItemCooldownEffect.java
@@ -1,37 +1,32 @@
 package com.nisovin.magicspells.spelleffects.effecttypes;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.configuration.ConfigurationSection;
 
 import com.nisovin.magicspells.util.TimeUtil;
 import com.nisovin.magicspells.util.SpellData;
 import com.nisovin.magicspells.util.config.ConfigData;
 import com.nisovin.magicspells.spelleffects.SpellEffect;
-import com.nisovin.magicspells.util.magicitems.MagicItem;
 import com.nisovin.magicspells.util.config.ConfigDataUtil;
-import com.nisovin.magicspells.util.magicitems.MagicItems;
 
 public class ItemCooldownEffect extends SpellEffect {
 
-	private ItemStack item;
-
 	private ConfigData<Integer> duration;
+	private ConfigData<Material> type;
 
 	@Override
 	protected void loadFromConfig(ConfigurationSection config) {
-		MagicItem magicItem = MagicItems.getMagicItemFromString(config.getString("item", "stone"));
-		if (magicItem != null) item = magicItem.getItemStack();
-
 		duration = ConfigDataUtil.getInteger(config, "duration", TimeUtil.TICKS_PER_SECOND);
+		type = ConfigDataUtil.getEnum(config, "item", Material.class, Material.STONE);
 	}
 
 	@Override
 	protected Runnable playEffectEntity(Entity entity, SpellData data) {
-		if (item == null || !(entity instanceof Player p)) return null;
+		if (!(entity instanceof Player player)) return null;
 
-		p.setCooldown(item.getType(), duration.get(data));
+		player.setCooldown(type.get(data), duration.get(data));
 		return null;
 	}
 
