fdf6ea23d1d528d90455eda240fbaaee675ec891
JasperLorelai <wolf.haci12@gmail.com>
2022-08-07 16:11:01 +0200

    Add relational operators

--- core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/PotionEffectCondition.java
+++ core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/PotionEffectCondition.java
@@ -6,50 +6,55 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import com.nisovin.magicspells.util.Util;
-import com.nisovin.magicspells.castmodifiers.Condition;
+import com.nisovin.magicspells.castmodifiers.conditions.util.OperatorCondition;
 
-public class PotionEffectCondition extends Condition {
+public class PotionEffectCondition extends OperatorCondition {
 
 	private PotionEffectType effectType;
 	private int value;
-	
+	private boolean matchOnlyType;
+
 	@Override
 	public boolean initialize(String var) {
-		if (var.contains(":")) {
-			String[] s = var.split(":");
-			effectType = Util.getPotionEffectType(s[0]);
+		String[] splits = var.split("[:=<>]");
+		if (splits.length > 1) {
+			if (splits[1].length() < 2 || !super.initialize(splits[1])) return false;
 			try {
-				value = Integer.parseInt(s[1]);
-			} catch (Exception e) {
+				value = Integer.parseInt(splits[1].substring(1));
+			} catch (NumberFormatException ignored) {
 				return false;
 			}
-		} else {
-			effectType = Util.getPotionEffectType(var);
-			value = -1;
 		}
+		else matchOnlyType = true;
+		effectType = Util.getPotionEffectType(splits[0]);
 		return effectType != null;
 	}
 
 	@Override
 	public boolean check(LivingEntity livingEntity) {
-		return check(livingEntity, livingEntity);
+		return checkPotionEffects(livingEntity);
 	}
 
 	@Override
 	public boolean check(LivingEntity livingEntity, LivingEntity target) {
-		boolean has = target.hasPotionEffect(effectType);
-		if (value == -1) return has;
-		if (has) {
-			for (PotionEffect effect : target.getActivePotionEffects()) {
-				if (effect.getType() == effectType) return effect.getAmplifier() == value;
-			}
-		}
-		return false;
+		return checkPotionEffects(target);
 	}
-	
+
 	@Override
 	public boolean check(LivingEntity livingEntity, Location location) {
 		return false;
-	}	
-	
+	}
+
+	private boolean checkPotionEffects(LivingEntity target) {
+		for (PotionEffect effect : target.getActivePotionEffects()) {
+			if (effect.getType() != effectType) continue;
+			if (matchOnlyType) return true;
+			int amplifier = effect.getAmplifier();
+			if (equals) return amplifier == value;
+			else if (moreThan) return amplifier > value;
+			else if (lessThan) return amplifier < value;
+		}
+		return false;
+	}
+
 }
