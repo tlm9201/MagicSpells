57d6742f807319964e73d1a1d271439c18709b0b
Chronoken <norwinofficial@gmail.com>
2022-12-15 19:58:15 +0100

    Add `SpellbookReloadEvent`, it is triggered when you reload a spellbook for a player.

---
+++ core/src/main/java/com/nisovin/magicspells/events/SpellbookReloadEvent.java
@@ -0,0 +1,39 @@
+package com.nisovin.magicspells.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+
+import com.nisovin.magicspells.Spellbook;
+
+public class SpellbookReloadEvent extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+
+	private Player player;
+
+	private Spellbook spellBook;
+
+	public SpellbookReloadEvent(Player player, Spellbook spellbook) {
+		this.player = player;
+		this.spellBook = spellbook;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public Spellbook getSpellBook() {
+		return spellBook;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+}
