f80a7982b210d01beedeb0f9a777176cf03c24fb
JasperLorelai <wolf.haci12@gmail.com>
2023-07-30 00:24:33 +0200

    feat: Add light type to ConditionVar

--- core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/LightLevelCondition.java
+++ core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/LightLevelCondition.java
@@ -1,6 +1,7 @@
 package com.nisovin.magicspells.castmodifiers.conditions;
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.LivingEntity;
 
 import com.nisovin.magicspells.handlers.DebugHandler;
@@ -8,10 +9,23 @@ import com.nisovin.magicspells.castmodifiers.conditions.util.OperatorCondition;
 
 public class LightLevelCondition extends OperatorCondition {
 
+	private LightType type = LightType.ALL;
 	private byte level = 0;
 
 	@Override
 	public boolean initialize(String var) {
+		if (var == null || var.isEmpty()) return false;
+		String[] splits = var.split(";");
+		if (splits.length > 1) {
+			try {
+				type = LightType.valueOf(splits[0].toUpperCase());
+			} catch (IllegalArgumentException e) {
+				DebugHandler.debugBadEnumValue(LightType.class, splits[0]);
+				return false;
+			}
+			var = splits[1];
+		}
+
 		if (var.length() < 2 || !super.initialize(var)) return false;
 
 		try {
@@ -39,10 +53,22 @@ public class LightLevelCondition extends OperatorCondition {
 	}
 
 	private boolean lightLevel(Location location) {
-		if (equals) return location.getBlock().getLightLevel() == level;
-		else if (moreThan) return location.getBlock().getLightLevel() > level;
-		else if (lessThan) return location.getBlock().getLightLevel() < level;
+		Block block = location.getBlock();
+		byte lightLevel = switch (type) {
+			case ALL -> block.getLightLevel();
+			case BLOCK -> block.getLightFromBlocks();
+			case SKY -> block.getLightFromSky();
+		};
+		if (equals) return lightLevel == level;
+		else if (moreThan) return lightLevel > level;
+		else if (lessThan) return lightLevel < level;
 		return false;
+	}
+
+	public enum LightType {
+		ALL,
+		BLOCK,
+		SKY
 	}
 
 }
