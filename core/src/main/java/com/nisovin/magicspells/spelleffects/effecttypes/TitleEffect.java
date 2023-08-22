f9ed5d6145b61792bd20f8ae4b2b87736038e4a5
TonytheMacaroni <tonythemacaroni123@gmail.com>
2023-08-21 22:01:00 -0400

    Variable replacement for timing

--- core/src/main/java/com/nisovin/magicspells/spelleffects/effecttypes/TitleEffect.java
+++ core/src/main/java/com/nisovin/magicspells/spelleffects/effecttypes/TitleEffect.java
@@ -20,10 +20,11 @@ public class TitleEffect extends SpellEffect {
 
 	private String title;
 	private String subtitle;
-	private Title.Times times;
+	private ConfigData<Title.Times> times;
 	private ConfigData<Boolean> broadcast;
+	private ConfigData<Boolean> useViewerAsTarget;
 
-	private static Duration milisOfTicks(int ticks) {
+	private static Duration millisOfTicks(int ticks) {
 		return Duration.ofMillis(TimeUtil.MILLISECONDS_PER_SECOND * (ticks / TimeUtil.TICKS_PER_SECOND));
 	}
 
@@ -32,25 +33,42 @@ public class TitleEffect extends SpellEffect {
 		title = config.getString("title", "");
 		subtitle = config.getString("subtitle", "");
 
-		int fadeIn = config.getInt("fade-in", 10);
-		int stay = config.getInt("stay", 40);
-		int fadeOut = config.getInt("fade-out", 10);
-		times = Title.Times.times(milisOfTicks(fadeIn), milisOfTicks(stay), milisOfTicks(fadeOut));
+		ConfigData<Integer> fadeIn = ConfigDataUtil.getInteger(config, "fade-in", 10);
+		ConfigData<Integer> stay = ConfigDataUtil.getInteger(config, "stay", 40);
+		ConfigData<Integer> fadeOut = ConfigDataUtil.getInteger(config, "fade-out", 10);
+		if (fadeIn.isConstant() && stay.isConstant() && fadeOut.isConstant()) {
+			Title.Times times = Title.Times.times(
+				millisOfTicks(fadeIn.get()),
+				millisOfTicks(stay.get()),
+				millisOfTicks(fadeOut.get())
+			);
+
+			this.times = data -> times;
+		} else {
+			times = data -> Title.Times.times(
+				millisOfTicks(fadeIn.get(data)),
+				millisOfTicks(stay.get(data)),
+				millisOfTicks(fadeOut.get(data))
+			);
+		}
 
 		broadcast = ConfigDataUtil.getBoolean(config, "broadcast", false);
+		useViewerAsTarget = ConfigDataUtil.getBoolean(config, "use-viewer-as-target", false);
 	}
 
 	@Override
 	protected Runnable playEffectEntity(Entity entity, SpellData data) {
-		if (broadcast.get(data)) Util.forEachPlayerOnline(p -> send(p, data.args()));
-		else if (entity instanceof Player player) send(player, data.args());
+		if (broadcast.get(data)) Util.forEachPlayerOnline(player -> send(player, data));
+		else if (entity instanceof Player player) send(player, data);
 		return null;
 	}
-	
-	private void send(Player player, String[] args) {
-		Component titleComponent = Util.getMiniMessageWithArgsAndVars(player, title, args);
-		Component subtitleComponent = Util.getMiniMessageWithArgsAndVars(player, subtitle, args);
-		player.showTitle(Title.title(titleComponent, subtitleComponent, times));
+
+	private void send(Player player, SpellData data) {
+		if (useViewerAsTarget.get(data)) data = data.target(player);
+
+		Component titleComponent = Util.getMiniMessage(title, player, data);
+		Component subtitleComponent = Util.getMiniMessage(subtitle, player, data);
+		player.showTitle(Title.title(titleComponent, subtitleComponent, times.get(data)));
 	}
 
 }
