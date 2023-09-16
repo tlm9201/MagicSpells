1ea2c26891a068feb28e35cc21667c4d3f128016
TonytheMacaroni <tonythemacaroni123@gmail.com>
2023-09-16 13:42:27 -0400

    Add method with fallback

--- core/src/main/java/com/nisovin/magicspells/util/config/ConfigData.java
+++ core/src/main/java/com/nisovin/magicspells/util/config/ConfigData.java
@@ -1,6 +1,7 @@
 package com.nisovin.magicspells.util.config;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import com.nisovin.magicspells.util.SpellData;
 
@@ -10,6 +11,11 @@ public interface ConfigData<T> {
 
 	default T get() {
 		return get(SpellData.NULL);
+	}
+
+	default T getOr(@NotNull SpellData data, @Nullable T fallback) {
+		T value = get(data);
+		return value == null ? fallback : value;
 	}
 
 	default boolean isConstant() {
