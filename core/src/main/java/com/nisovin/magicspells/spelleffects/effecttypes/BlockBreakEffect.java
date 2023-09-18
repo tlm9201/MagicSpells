7407f79222fecf15130bb4fee052dfb03c984ecf
TonytheMacaroni <tonythemacaroni123@gmail.com>
2023-09-18 16:55:33 -0400

    Match stages as specified in protocol

--- core/src/main/java/com/nisovin/magicspells/spelleffects/effecttypes/BlockBreakEffect.java
+++ core/src/main/java/com/nisovin/magicspells/spelleffects/effecttypes/BlockBreakEffect.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.configuration.ConfigurationSection;
 
+import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.util.SpellData;
 import com.nisovin.magicspells.util.config.ConfigData;
 import com.nisovin.magicspells.spelleffects.SpellEffect;
@@ -11,29 +12,24 @@ import com.nisovin.magicspells.util.config.ConfigDataUtil;
 
 public class BlockBreakEffect extends SpellEffect {
 
-	private static final int VANILLA_RADIUS = 32;
-
 	private static int sourceId = 0;
 
 	private ConfigData<Integer> range;
 	private ConfigData<Integer> stage;
-
 	@Override
 	public void loadFromConfig(ConfigurationSection config) {
-		range = ConfigDataUtil.getInteger(config, "range", VANILLA_RADIUS);
-		stage = ConfigDataUtil.getInteger(config, "stage", 1);
+		range = ConfigDataUtil.getInteger(config, "range", 32);
+		stage = ConfigDataUtil.getInteger(config, "stage", 0);
 	}
 
 	@Override
 	public Runnable playEffectLocation(Location location, SpellData data) {
-		if (data == null) return null;
-		if (location.getBlock().getType().isAir()) return null;
+		int stage = this.stage.get(data);
+		float progress = (stage >= 0 && stage <= 9) ? Math.min((stage + 1) / 10f, 1) : 0f;
 
-		double radius = Math.min(range.get(data), VANILLA_RADIUS);
-		float progress = Math.max(0, Math.min(1, stage.get(data) / 10F));
-		for (Player player : location.getNearbyPlayers(radius)) {
-			player.sendBlockDamage(location, progress, sourceId++);
-		}
+		double range = Math.min(this.range.get(data), MagicSpells.getGlobalRadius());
+		for (Player player : location.getNearbyPlayers(range))
+			player.sendBlockDamage(location, progress, --sourceId);
 
 		return null;
 	}
