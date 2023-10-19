51be3c1a92f765ca453ee0230bcbd243894b149f
Chronoken <norwinofficial@gmail.com>
2023-10-19 21:10:24 +0200

    Bump to beta 14.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,6 @@ include("teams")
 include("towny")
 
 include(":nms:shared")
-include(":nms:v1_19_R1")
-include(":nms:v1_19_R2")
 include(":nms:v1_19_R3")
 include(":nms:v1_20_R1")
 include(":nms:v1_20_R2")
