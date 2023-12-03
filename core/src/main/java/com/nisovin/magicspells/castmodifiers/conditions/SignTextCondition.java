b9b0002e75256d29f7b07677b7a0e8ddc21cd946
JasperLorelai <wolf.haci12@gmail.com>
2023-12-03 21:50:45 +0100

    fix: signtext condition
    * Added support for prepending the format with "front;" or "back;" to define what side
    * The condition compared Component objects directly. That's bad because when deserialised, Component objects may differ even if their text was the same. The comparison was changed to strict MiniMessage strings instead.
    * The "world,x,y,z" format is now optional as the condition can now check for the caster's/target's location instead.

--- core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/SignTextCondition.java
+++ core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/SignTextCondition.java
@@ -2,10 +2,13 @@ package com.nisovin.magicspells.castmodifiers.conditions;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.bukkit.Location;
 import org.bukkit.block.Sign;
 import org.bukkit.block.Block;
+import org.bukkit.block.sign.Side;
 import org.bukkit.entity.LivingEntity;
 
 import net.kyori.adventure.text.Component;
@@ -15,53 +18,71 @@ import com.nisovin.magicspells.util.MagicLocation;
 import com.nisovin.magicspells.handlers.DebugHandler;
 import com.nisovin.magicspells.castmodifiers.Condition;
 
+/*
+ * Format: side;world,x,y,z,line__1\nline__2
+ * "side" is optional and can be "front" (def) or "back".
+ * world,x,y,z is optional, "x,y,z" must be integers.
+ * "lines" should follow strict MiniMessage format.
+ */
 public class SignTextCondition extends Condition {
 
-	//world,x,y,z,text
+	private static final Pattern FORMAT = Pattern.compile("(?:(?<side>front|back);)?(?:(?<world>[^,]+),(?<x>-?\\d+),(?<y>-?\\d+),(?<z>-?\\d+),)?(?<lines>.+)", Pattern.DOTALL);
 
+	private Side side = Side.FRONT;
 	private MagicLocation location;
-	private List<Component> text;
+	private final List<String> text = new ArrayList<>();
 
 	@Override
 	public boolean initialize(String var) {
-		try {
-			String[] vars = var.split(",");
-			location = new MagicLocation(vars[0], Integer.parseInt(vars[1]), Integer.parseInt(vars[2]), Integer.parseInt(vars[3]));
+		Matcher matcher = FORMAT.matcher(var);
+		if (!matcher.find()) return false;
+		String sideName = matcher.group("side");
+		String world = matcher.group("world");
+		String x = matcher.group("x");
+		String y = matcher.group("y");
+		String z = matcher.group("z");
+		String lines = matcher.group("lines");
 
-			text = new ArrayList<>();
-			for (String line : vars[4].split("\\\\n")) {
-				text.add(Util.getLegacyFromString(line.replaceAll("__", " ")));
+		if (sideName != null && sideName.equals("back")) side = Side.BACK;
+		if (world != null && x != null && y != null && z != null) {
+			try {
+				location = new MagicLocation(world, Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(z));
+			} catch (NumberFormatException e) {
+				DebugHandler.debugNumberFormat(e);
+				return false;
 			}
-			return true;
-		} catch (Exception e) {
-			DebugHandler.debugGeneral(e);
-			return false;
 		}
+		for (String line : lines.split("\\\\n|\\n")) {
+			text.add(line.replaceAll("__", " "));
+		}
+		return true;
 	}
 
 	@Override
 	public boolean check(LivingEntity caster) {
-		return checkSignText();
+		return checkSignText(caster.getLocation());
 	}
 
 	@Override
 	public boolean check(LivingEntity caster, LivingEntity target) {
-		return checkSignText();
+		return checkSignText(target.getLocation());
 	}
 
 	@Override
 	public boolean check(LivingEntity caster, Location location) {
-		return checkSignText();
+		return checkSignText(location);
 	}
 
-	public boolean checkSignText() {
-		Block block = location.getLocation().getBlock();
+	public boolean checkSignText(Location targetedLocation) {
+		Location signLocation = location == null ? targetedLocation : location.getLocation();
+		Block block = signLocation.getBlock();
 		if (!block.getType().name().contains("SIGN")) return false;
 
-		Sign sign = (Sign) block.getState();
-		List<Component> lines = sign.lines();
+		List<Component> lines = ((Sign) block.getState()).getSide(side).lines();
 		for (int i = 0; i < lines.size(); i++) {
-			if (lines.get(i).equals(text.get(i))) continue;
+			String signLine = Util.getStrictStringFromComponent(lines.get(i));
+			String checkLine = text.size() - 1 >= i ? text.get(i) : "";
+			if (signLine.equals(checkLine)) continue;
 			return false;
 		}
 
