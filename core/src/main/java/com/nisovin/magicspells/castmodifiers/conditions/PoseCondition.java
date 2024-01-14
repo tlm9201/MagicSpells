09d03d854f48a4edaa4bc4d140217d19e4a45ad1
TonytheMacaroni <tonythemacaroni123@gmail.com>
2024-01-14 18:07:40 -0500

    Add pose and fixedpose conditions

---
+++ core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/PoseCondition.java
@@ -0,0 +1,46 @@
+package com.nisovin.magicspells.castmodifiers.conditions;
+
+import java.util.Set;
+import java.util.EnumSet;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Pose;
+import org.bukkit.entity.LivingEntity;
+
+import com.nisovin.magicspells.castmodifiers.Condition;
+
+public class PoseCondition extends Condition {
+
+	private final Set<Pose> poses = EnumSet.noneOf(Pose.class);
+
+	@Override
+	public boolean initialize(String var) {
+		String[] split = var.split(",");
+
+		for (String pose : split) {
+			try {
+				poses.add(Pose.valueOf(pose.trim().toUpperCase()));
+			} catch (IllegalArgumentException e) {
+				return false;
+			}
+		}
+
+		return !poses.isEmpty();
+	}
+
+	@Override
+	public boolean check(LivingEntity caster) {
+		return poses.contains(caster.getPose());
+	}
+
+	@Override
+	public boolean check(LivingEntity caster, LivingEntity target) {
+		return poses.contains(target.getPose());
+	}
+
+	@Override
+	public boolean check(LivingEntity caster, Location location) {
+		return false;
+	}
+
+}
