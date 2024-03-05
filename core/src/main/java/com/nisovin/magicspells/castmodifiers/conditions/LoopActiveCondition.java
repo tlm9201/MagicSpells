ed7c3238769aac33b22c97eba1d318c86f55d57e
TonytheMacaroni <tonythemacaroni123@gmail.com>
2024-03-05 13:55:38 -0500

    Add count to loop conditions

--- core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/LoopActiveCondition.java
+++ core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/LoopActiveCondition.java
@@ -1,5 +1,8 @@
 package com.nisovin.magicspells.castmodifiers.conditions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 
@@ -8,42 +11,64 @@ import org.jetbrains.annotations.NotNull;
 import com.nisovin.magicspells.Spell;
 import com.nisovin.magicspells.util.Name;
 import com.nisovin.magicspells.MagicSpells;
-import com.nisovin.magicspells.castmodifiers.Condition;
 import com.nisovin.magicspells.spells.targeted.LoopSpell;
+import com.nisovin.magicspells.castmodifiers.conditions.util.OperatorCondition;
 
 @Name("loopactive")
-public class LoopActiveCondition extends Condition {
+public class LoopActiveCondition extends OperatorCondition {
 
-	private LoopSpell loop;
+	private static final Pattern OPERATORS = Pattern.compile("[:=<>]");
+
+	protected LoopSpell loop;
+	protected int value;
 
 	@Override
 	public boolean initialize(@NotNull String var) {
 		Spell spell = MagicSpells.getSpellByInternalName(var);
 		if (spell instanceof LoopSpell loopSpell) {
 			loop = loopSpell;
+			moreThan = true;
+			value = 0;
+
 			return true;
 		}
 
-		return false;
+		Matcher matcher = OPERATORS.matcher(var);
+		while (matcher.find()) {
+			spell = MagicSpells.getSpellByInternalName(var.substring(0, matcher.start()));
+			if (!(spell instanceof LoopSpell loopSpell)) continue;
+
+			String number = var.substring(matcher.start());
+			if (number.length() < 2 || !super.initialize(number)) continue;
+
+			try {
+				value = Integer.parseInt(number.substring(1));
+			} catch (NumberFormatException e) {
+				continue;
+			}
+
+			loop = loopSpell;
+
+			return true;
+		}
+
+		return loop != null;
 	}
 
 	@Override
 	public boolean check(LivingEntity caster) {
-		return loopActive(caster);
+		int count = loop.getActiveLoops().get(caster.getUniqueId()).size();
+		return compare(count, value);
 	}
 
 	@Override
 	public boolean check(LivingEntity caster, LivingEntity target) {
-		return loopActive(target);
+		return check(target);
 	}
 
 	@Override
 	public boolean check(LivingEntity caster, Location location) {
 		return false;
-	}
-
-	private boolean loopActive(LivingEntity target) {
-		return loop.isActive(target);
 	}
 
 }
