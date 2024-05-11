6dd7a2978ed52d212f37ce9e130a0e8621ae887a
TonytheMacaroni <tonythemacaroni123@gmail.com>
2024-05-11 10:42:50 -0400

    Query for region in zone check to avoid stale region references

--- core/src/main/java/com/nisovin/magicspells/zones/NoMagicZoneWorldGuard.java
+++ core/src/main/java/com/nisovin/magicspells/zones/NoMagicZoneWorldGuard.java
@@ -1,18 +1,18 @@
 package com.nisovin.magicspells.zones;
 
 import org.bukkit.World;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
+
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.protection.regions.RegionType;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 import com.nisovin.magicspells.util.Name;
 import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.util.DependsOn;
-
-import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldedit.bukkit.BukkitAdapter;
-import com.sk89q.worldguard.protection.managers.RegionManager;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 @Name("worldguard")
 @DependsOn("WorldGuard")
@@ -21,37 +21,39 @@ public class NoMagicZoneWorldGuard extends NoMagicZone {
 	private String worldName;
 	private String regionName;
 
-	private ProtectedRegion region;
-
-	private boolean global = false;
-
 	@Override
 	public void initialize(ConfigurationSection config) {
-		worldName = config.getString("world", "");
-		regionName = config.getString("region", "");
-
-		World world = Bukkit.getWorld(worldName);
-		if (world == null) return;
-
-		RegionManager regionManager = WorldGuard.getInstance()
-				.getPlatform()
-				.getRegionContainer()
-				.get(BukkitAdapter.adapt(world));
-		if (regionManager != null) region = regionManager.getRegion(regionName);
-
-		if (regionName.toLowerCase().contains("global")) global = true;
+		worldName = config.getString("world");
+		regionName = config.getString("region");
 	}
 
 	@Override
 	public boolean inZone(Location location) {
-		if (!worldName.equals(location.getWorld().getName())) return false;
-		if (region == null && !global) {
-			MagicSpells.error("Failed to access WorldGuard region '" + regionName + "'");
+		if (regionName == null) {
+			MagicSpells.error("Failed to access WorldGuard region - no region specified.");
 			return false;
 		}
 
-		if (global) return true;
-		return region.contains(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+		if (worldName == null) {
+			MagicSpells.error("Failed to access WorldGuard region '" + regionName + "' - no world specified.");
+			return false;
+		}
+
+		World world = location.getWorld();
+		if (world == null || !world.getName().equals(worldName)) return false;
+
+		RegionManager regionManager = WorldGuard.getInstance()
+			.getPlatform()
+			.getRegionContainer()
+			.get(BukkitAdapter.adapt(world));
+
+		ProtectedRegion region = regionManager == null ? null : regionManager.getRegion(regionName);
+		if (region == null) {
+			MagicSpells.error("Failed to access WorldGuard region '" + regionName + "' - no such region exists.");
+			return false;
+		}
+
+		return region.getType() == RegionType.GLOBAL || region.contains(location.getBlockX(), location.getBlockY(), location.getBlockZ());
 	}
 
 }
