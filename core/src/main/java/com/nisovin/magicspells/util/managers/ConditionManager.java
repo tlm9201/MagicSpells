6b1297d0dde5b0cc1b411c4f58242ccc9962a271
JasperLorelai <wolf.haci12@gmail.com>
2024-12-19 00:13:41 +0100

    revert: Add "using" condition (675f9d21)
    Keep "usingitem" condition

--- core/src/main/java/com/nisovin/magicspells/util/managers/ConditionManager.java
+++ core/src/main/java/com/nisovin/magicspells/util/managers/ConditionManager.java
@@ -218,7 +218,7 @@ public class ConditionManager {
 		addCondition(SleepingCondition.class);
 		addCondition(UnderWaterCondition.class);
 		addCondition(FixedTimeCondition.class);
-		addCondition(UsingCondition.class);
+		addCondition(UsingItemCondition.class);
 	}
 
 }
