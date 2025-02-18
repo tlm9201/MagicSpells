f2c3998da2fb3418bcb27b7ee74974756673306a
JasperLorelai <wolf.haci12@gmail.com>
2025-02-18 23:23:07 +0100

    chore: Deprecate unused interface

--- core/src/main/java/com/nisovin/magicspells/spells/DamageSpell.java
+++ core/src/main/java/com/nisovin/magicspells/spells/DamageSpell.java
@@ -1,7 +1,14 @@
 package com.nisovin.magicspells.spells;
 
+import com.nisovin.magicspells.events.SpellApplyDamageEvent;
+
+/**
+ * @deprecated "Spell damage type" string is now on {@link SpellApplyDamageEvent}.
+ */
+@Deprecated(forRemoval = true)
 public interface DamageSpell {
 
+	@Deprecated(forRemoval = true)
 	String getSpellDamageType();
 
 }
